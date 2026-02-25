ed86801584c1e46e5e35ceec119f4699764c4630
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-25 22:39:39 +0530

    Updated navbar styling

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://url-shortener-backend-yvyu.onrender.com/v1/',
+    baseURL: 'https://url-shortener-backend-yvyu.onrender.com/v1',
     withCredentials: true
 })
 
