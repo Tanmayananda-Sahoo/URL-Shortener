07fc00ba6e3fdc94328455386699d97cffa11e17
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-25 22:30:19 +0530

    final commit after deploy

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://url-shortener-backend-yvyu.onrender.com',
         credentials: true
     }
 ));
