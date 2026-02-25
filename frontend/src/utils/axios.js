a9b5220590178d224401e5b2921083c7c70f47be
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-25 22:25:13 +0530

    i guess the final one

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/v1/',
+    baseURL: 'https://url-shortener-backend-yvyu.onrender.com/v1/',
     withCredentials: true
 })
 
