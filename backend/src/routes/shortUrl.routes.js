8947de3faae6f4bb22959cbc509dc938644e72c2
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-25 22:48:10 +0530

    final commit first error

--- backend/src/routes/shortUrl.routes.js
+++ backend/src/routes/shortUrl.routes.js
@@ -5,7 +5,7 @@ import authenticateUser from '../middlewares/auth.middlewares.js';
 const router = Router();
 
 router.route('/generate').post(authenticateUser, generateShortUrl);
-router.route('/:uniqueId').get(authenticateUser, redirectToOriginalUrl);
+router.route('/:uniqueId').get(redirectToOriginalUrl);
 router.route('/fetch/urls').get(authenticateUser, fetchUrls);
 
 export default router;
